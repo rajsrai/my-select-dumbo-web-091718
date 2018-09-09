60ffccfe193a5de0a7a9aa23fb15fa8eefeea470
Rajvir Rai <rsr1992@gmail.com>
2018-09-09 23:43:41 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,11 @@
-def my_select(collection)
+def my_select(array)
  x = 0 
  arr = []
  while x < array.size 
- if yield(array[count]) == true
-  result.push(array[count])
+ if yield(array[x]) == true
+  result.push(array[x])
 end
-  count+=1
+  x+=1
 end
 result
 end
